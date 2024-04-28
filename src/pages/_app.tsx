c98f68e6af0e794e26a99bf401ca53de5e152849
Janos Feher <feherjani96@gmail.com>
2024-04-28 16:57:05 +0200

    fix mobile viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,10 @@ import PlausibleProvider from "next-plausible";
 import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import SEO from "../../next-seo.config";
-import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
-      </Head>
       <DefaultSeo {...SEO} />
       <PlausibleProvider
         domain="kir-dev.hu"
